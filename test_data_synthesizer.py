85f296824e7a22c84e5f5208b516459722aed6a3
FinTechInsider <thefintechinsider@gmail.com>
2020-09-18 12:42:41 -0400

    synthesizer

---
+++ test_data_synthesizer.py
@@ -0,0 +1,20 @@
+import random
+import uuid 
+  
+outF = open("input_data.txt", "w")
+for i in range(500):
+    id_type = random.choice(['BBG', 'ISIN', 'UUID', 'DTCCC'])
+    id = None
+
+    if id_type == 'BBG':
+        id = 'BBG{}'.format('{:05}'.format(abs(random.randint(0, 10000))))
+    elif id_type == 'ISIN':
+        id = 'US{}'.format('{:08}'.format(abs(random.randint(0, 10000))))
+    elif id_type == 'DTCCC':
+        id = '{}'.format('{:10}'.format(abs(random.randint(0, 10000))))
+    elif id_type == 'UUID':
+        id = str(uuid.uuid1())
+
+    outF.write("{},{}\n".format(id, id_type))
+
+outF.close()
